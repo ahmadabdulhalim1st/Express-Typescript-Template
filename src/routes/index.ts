1be2945e0e3ad957a0247196e670e034d307e419
ahmadabdulhalim1st <ahmedabdelhaliem90@gmail.com>
2026-09-02 12:07:08 +0300

    Public

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,1 @@
-export { default as authRouter } from "@routes/auth.route";
-export { default as userRouter } from "@routes/user.route";
-export { default as adminRouter } from "@routes/admin.route";
+export { default as authRouter } from "@modules/auth/auth.route";
